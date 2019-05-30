6294f19fb0b7e8129413df7269f92d3ea8ff31a2
Deepankar Agrawal <deepankar11093@gmail.com>
2019-05-31 04:20:46 +0530

    remove unused dep

--- tdnn_wrapper.h
+++ tdnn_wrapper.h
@@ -3,8 +3,6 @@
 #include "feat/wave-reader.h"
 #include "online2/online-nnet3-decoding.h"
 #include "online2/online-nnet2-feature-pipeline.h"
-#include "online2/onlinebin-util.h"
-#include "online2/online-endpoint.h"
 #include "fstext/fstext-lib.h"
 #include "lat/lattice-functions.h"
 #include "lat/kaldi-lattice.h"
