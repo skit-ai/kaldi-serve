5c8270fbad7df97762d222197349c7cb17f8329c
Prabhsimran Singh <pskrunner14@gmail.com>
2019-06-24 17:44:04 +0530

    some minor refactor: shaved off some time per req

--- src/kaldi_serve_app.cc
+++ src/kaldi_serve_app.cc
@@ -50,8 +50,9 @@ using kaldi_serve::SpeechRecognitionAlternative;
 using kaldi_serve::SpeechRecognitionResult;
 
 using namespace std;
-using std::chrono::system_clock;
 
+using std::chrono::system_clock;
+using std::chrono::high_resolution_clock;
 
 // An alternative is a pair of string (hypothesis) and it's confidence
 using alternative_t = std::pair<std::string, double>;
@@ -295,17 +296,18 @@ public:
     decode_fst = fst::ReadFstKaldiGeneric(hclg_filepath);
   }
 
-  Decoder* operator()(kaldi::BaseFloat beam, std::size_t max_active,
+  shared_ptr<Decoder> operator()(kaldi::BaseFloat beam, std::size_t max_active,
                       std::size_t min_active, kaldi::BaseFloat lattice_beam,
                       kaldi::BaseFloat acoustic_scale, std::size_t frame_subsampling_factor,
                       std::string word_syms_filepath,
                       std::string model_filepath,
                       std::string mfcc_conf_filepath,
                       std::string ie_conf_filepath) {
-    return new Decoder(beam, max_active, min_active, lattice_beam,
-                       acoustic_scale, frame_subsampling_factor,
-                       word_syms_filepath, model_filepath,
-                       mfcc_conf_filepath, ie_conf_filepath, decode_fst);
+    return shared_ptr<Decoder>(new Decoder(beam, max_active, min_active, lattice_beam,
+                                           acoustic_scale, frame_subsampling_factor,
+                                           word_syms_filepath, model_filepath,
+                                           mfcc_conf_filepath, ie_conf_filepath, 
+                                           decode_fst));
   }
 
   ~DecoderFactory() {
@@ -316,70 +318,85 @@ private:
   const fst::Fst<fst::StdArc>* decode_fst;
 };
 
-void transcribe(const RecognitionConfig* config, const RecognitionAudio* audio,
-                const string uuid, RecognizeResponse* recognizeResponse, 
-                const shared_ptr<Decoder> &decoder){
-  std::cout << "UUID: \t" << uuid << std::endl;
 
-  std::cout << "Encoding: " << config->encoding() << std::endl;
-  std::cout << "Sample Rate Hertz: " << config->sample_rate_hertz() << std::endl;
-  std::cout << "Language Code: " << config->language_code() << std::endl;
-  std::cout << "Max Alternatives: " << config->max_alternatives() << std::endl;
-  std::cout << "Punctuation: " << config->punctuation() << std::endl;
-  std::cout << "Model: " << config->model() << std::endl;
+class KaldiServeImpl final : public kaldi_serve::KaldiServe::Service {
 
-  std::cout << "NOTE: We are ignoring most of the parameters, other than the audio bytes, for now."
-            << std::endl;
-
-  SpeechRecognitionResult* results = recognizeResponse->add_results();
-
-  int32 n_best = config->max_alternatives();
-  std::stringstream input_stream(audio->content());
-
-  SpeechRecognitionAlternative* alternative;
-  for (auto const &res : decoder->decode_stream(input_stream, n_best)) {
-    alternative = results->add_alternatives();
-    alternative->set_transcript(res.first.first);
-    alternative->set_confidence(res.first.second);
-  }
-
-  return;
-}
-
-
-class KaldiServeImpl final : public KaldiServe::Service {
 private:
-  shared_ptr<Decoder> decoder;
+  // decoder object (read-only)
+  const shared_ptr<Decoder> decoder;
+
+  void transcribe(const kaldi_serve::RecognitionConfig* config, 
+                  const kaldi_serve::RecognitionAudio* audio,
+                  const std::string &uuid, 
+                  kaldi_serve::RecognizeResponse* recognizeResponse){
+
+    std::cout << "UUID: \t" << uuid << std::endl;
+
+    std::cout << "Encoding: " << config->encoding() << std::endl;
+    std::cout << "Sample Rate Hertz: " << config->sample_rate_hertz() << std::endl;
+    std::cout << "Language Code: " << config->language_code() << std::endl;
+    std::cout << "Max Alternatives: " << config->max_alternatives() << std::endl;
+    std::cout << "Punctuation: " << config->punctuation() << std::endl;
+    std::cout << "Model: " << config->model() << std::endl;
+
+    std::cout << "NOTE: We are ignoring most of the parameters, other than the audio bytes, for now."
+              << std::endl;
+
+    kaldi_serve::SpeechRecognitionResult* results = recognizeResponse->add_results();
+
+    int32 n_best = config->max_alternatives();
+    std::stringstream input_stream(audio->content());
+
+    kaldi_serve::SpeechRecognitionAlternative* alternative;
+    for (auto const &res : this->decoder->decode_stream(input_stream, n_best)) {
+      alternative = results->add_alternatives();
+      alternative->set_transcript(res.first.first);
+      alternative->set_confidence(res.first.second);
+    }
+
+    return;
+  }
 
 public:
-  explicit KaldiServeImpl(shared_ptr<Decoder> decoder): decoder(decoder) {
-  }
+  explicit KaldiServeImpl(const shared_ptr<Decoder> &decoder): decoder(decoder) {}
 
-  Status Recognize(ServerContext* context, const RecognizeRequest* recognizeRequest,
-                   RecognizeResponse* recognizeResponse) override {
-    RecognitionConfig config;
-    RecognitionAudio audio;
-    string uuid;
+  Status Recognize(grpc::ServerContext* context, 
+                   const kaldi_serve::RecognizeRequest* recognizeRequest,
+                   kaldi_serve::RecognizeResponse* recognizeResponse) override {
+    
+    // LOG REQUEST RESOLVE TIME --> START
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end; 
+    start = std::chrono::high_resolution_clock::now();
+    
+    kaldi_serve::RecognitionConfig config = recognizeRequest->config();
+    kaldi_serve::RecognitionAudio audio = recognizeRequest->audio();
+    std::string uuid = recognizeRequest->uuid();
 
-    config = recognizeRequest->config();
-    audio  = recognizeRequest->audio();
-    uuid   = recognizeRequest->uuid();
+    this->transcribe(&config, &audio, uuid, recognizeResponse);
 
-    transcribe(&config, &audio, uuid, recognizeResponse, this->decoder);
+    end = std::chrono::high_resolution_clock::now(); 
+    std::chrono::duration<double> res_duration = end - start; 
 
-    return Status::OK;
+    std::cout << "request resolved in: " << res_duration.count() << 's' << std::endl;
+    // LOG REQUEST RESOLVE TIME --> END
+
+    return grpc::Status::OK;
   }
 };
 
 
 void run_server(const shared_ptr<Decoder> &decoder) {
-  std::string server_address("0.0.0.0:5016");
+  // define a kaldi serve instance and pass the decoder
   KaldiServeImpl service(decoder);
 
-  ServerBuilder builder;
+  std::string server_address("0.0.0.0:5016");
+
+  grpc::ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
   builder.RegisterService(&service);
-  std::unique_ptr<Server> server(builder.BuildAndStart());
+  
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+
   std::cout << "Server listening on " << server_address << std::endl;
   server->Wait();
 }
@@ -403,12 +420,23 @@ int main(int argc, char* argv[]) {
   std::string mfcc_conf_filepath = model_dir + "/tdnn1g_sp_online/conf/mfcc.conf";
   std::string ivec_conf_filepath = model_dir + "/tdnn1g_sp_online/conf/ivector_extractor.conf";
 
-  DecoderFactory decoder_factory(hclg_filepath);
-  shared_ptr<Decoder> decoder = shared_ptr<Decoder>(decoder_factory(13.0, 7000, 200, 6.0, 1.0, 3,
-                                                                    words_filepath, model_filepath,
-                                                                    mfcc_conf_filepath, 
-                                                                    ivec_conf_filepath));
+  // LOG MODEL LOAD TIME --> START
+  std::chrono::time_point<std::chrono::high_resolution_clock> start, end; 
+  start = std::chrono::high_resolution_clock::now();
 
+  DecoderFactory decoder_factory(hclg_filepath);
+  shared_ptr<Decoder> decoder = decoder_factory(13.0, 7000, 200, 6.0, 1.0, 3,
+                                                words_filepath, model_filepath,
+                                                mfcc_conf_filepath, ivec_conf_filepath);
+
+  end = std::chrono::high_resolution_clock::now(); 
+  std::chrono::duration<double> load_duration = end - start; 
+
+  std::cout << "decoder model loaded in: " << load_duration.count() << 's' << std::endl;
+  // LOG MODEL LOAD TIME --> END
+
+  // runs the server using the loaded decoder model
   run_server(decoder);
+
   return 0;
 }
