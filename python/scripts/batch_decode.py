434f427951d5216bc6ce1e520782f1897c9b3ea2
Prabhsimran Singh <pskrunner14@gmail.com>
2019-09-29 17:27:34 +0530

    added batch decode script

--- python/scripts/batch_decode.py
+++ python/scripts/batch_decode.py
@@ -1,32 +1,32 @@
 """
-Script for testing out ASR server.
+Script for transcribing audios in batches using Kaldi-Serve ASR server.
 
 Usage:
-  example_client.py mic [--n-secs=<n-secs>] [--model=<model>] [--lang=<lang>] [--raw]
-  example_client.py <file>... [--model=<model>] [--lang=<lang>] [--raw]
+  batch_decode.py <audio_paths_file> [--model=<model>] [--lang=<lang>] [--num-proc=<num-proc>] [--output-json=<output-json>] [--raw]
 
 Options:
-  --n-secs=<n-secs>     Number of seconds to records, ideally there should be a VAD here. [default: 3]
-  --model=<model>       Name of the model to hit [default: general]
-  --lang=<lang>         Language code of the model [default: hi]
-  --raw                 Flag that specifies whether to stream raw audio bytes to server.
+  --model=<model>               Name of the model to hit [default: general]
+  --lang=<lang>                 Language code of the model [default: hi]
+  --num-proc=<num-proc>         Number of parallel processes [default: 8]
+  --output-json=<output-json>   Output json file path for decoded transcriptions [default: transcripts.json]
+  --raw                         Flag that specifies whether to stream raw audio bytes to server.
 """
 
-import random
-import threading
+import json
 import traceback
-from pprint import pprint
-from typing import List
 
+from typing import List
 from docopt import docopt
 
+from multiprocessing import Pool
+
 from kaldi_serve import KaldiServeClient, RecognitionAudio, RecognitionConfig
-from kaldi_serve.utils import (chunks_from_file, chunks_from_mic,
-                               raw_bytes_to_wav)
+from kaldi_serve.utils import chunks_from_file, raw_bytes_to_wav
 
 SR = 8000
 CHANNELS = 1
 
+client = KaldiServeClient(kaldi_serve_url="172.31.83.213:5016")
 
 def parse_response(response):
     output = []
@@ -44,17 +44,17 @@ def parse_response(response):
     return output
 
 
-def transcribe_chunks(client, audio_chunks, model: str, language_code: str, raw: bool=False):
+def transcribe_chunks(audio_chunks, model: str, language_code: str, raw: bool=False):
     """
     Transcribe the given audio chunks
     """
+    global client
 
     response = {}
     encoding = RecognitionConfig.AudioEncoding.LINEAR16
 
     try:
         if raw:
-            print('streaming raw')
             config = lambda chunk_len: RecognitionConfig(
                 sample_rate_hertz=SR,
                 encoding=encoding,
@@ -67,7 +67,6 @@ def transcribe_chunks(client, audio_chunks, model: str, language_code: str, raw:
             audio_params = [(config(len(chunk)), RecognitionAudio(content=chunk)) for chunk in audio_chunks]
             response = client.streaming_recognize_raw(audio_params, uuid="")
         else:
-            print('streaming with headers')
             audio = (RecognitionAudio(content=chunk) for chunk in audio_chunks)
             config = RecognitionConfig(
                 sample_rate_hertz=SR,
@@ -80,37 +79,45 @@ def transcribe_chunks(client, audio_chunks, model: str, language_code: str, raw:
     except Exception as e:
         traceback.print_exc()
         print(f'error: {str(e)}')
+        return None
 
-    pprint(parse_response(response))
+    return parse_response(response)
 
 
-def decode_files(client, audio_paths: List[str], model: str, language_code: str, raw: bool=False):
+def decode_audios(audio_paths: List[str], model: str, language_code: str, num_proc: int=8, raw: bool=False):
     """
     Decode files using threaded requests
     """
+    chunked_audios = [chunks_from_file(x, chunk_size=1, raw=raw) for x in audio_paths]
 
-    chunked_audios = [chunks_from_file(x, chunk_size=random.randint(1, 3), raw=raw) for x in audio_paths]
-
-    threads = [
-        threading.Thread(target=transcribe_chunks, args=(client, chunks, model, language_code, raw))
+    args = [
+        (chunks, model, language_code, raw)
         for chunks in chunked_audios
     ]
 
-    for thread in threads:
-        thread.start()
+    with Pool(num_proc) as pool:
+        results = pool.starmap(transcribe_chunks, args)
 
-    for thread in threads:
-        thread.join()
+    results_dict = {path: response for path, response in list(zip(audio_paths, results))}
+    return results_dict
 
 
 if __name__ == "__main__":
     args = docopt(__doc__)
-    client = KaldiServeClient()
+
     model = args["--model"]
     language_code = args["--lang"]
+    num_proc = int(args['--num-proc'])
+    output_json = args['--output-json']
     raw = args['--raw']
 
-    if args["mic"]:
-        transcribe_chunks(client, chunks_from_mic(int(args["--n-secs"]), SR, 1), model, language_code, raw)
-    else:
-        decode_files(client, args["<file>"], model, language_code, raw)
+    audio_paths_file = args["<audio_paths_file>"]
+    with open(audio_paths_file, 'r', encoding='utf-8') as f:
+        audio_paths = f.read().split('\n')
+
+    audio_paths = list(filter(lambda x: x.endswith(".wav"), audio_paths))
+
+    results_dict = decode_audios(audio_paths, model, language_code, num_proc=num_proc, raw=raw)
+    with open(output_json, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(results_dict))
+
