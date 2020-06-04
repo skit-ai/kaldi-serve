4fbeeea9165cfbea5288f95dfefbfe7abb2c15d6
Al Zatv <al.zatv@gmail.com>
2020-06-04 17:51:30 +0300

    1) Add make_unique() function, which is the only missing piece to make kaldi-serve on c++11 2) switch -std=c++17 to -std=c++11 in Makefile

--- src/config.hpp
+++ src/config.hpp
@@ -13,6 +13,47 @@
 // kaldi includes
 #include "base/kaldi-types.h"
 
+
+
+#if __cplusplus == 201103L
+  //make_unique() implementation for C++11 & 14 from https://stackoverflow.com/questions/17902405/how-to-implement-make-unique-function-in-c11/17902439#17902439
+  #include <cstddef>
+  #include <memory>
+  #include <type_traits>
+  #include <utility>
+
+  namespace std {
+    template<class T> struct _Unique_if {
+        typedef unique_ptr<T> _Single_object;
+    };
+
+    template<class T> struct _Unique_if<T[]> {
+        typedef unique_ptr<T[]> _Unknown_bound;
+    };
+
+    template<class T, size_t N> struct _Unique_if<T[N]> {
+        typedef void _Known_bound;
+    };
+
+    template<class T, class... Args>
+        typename _Unique_if<T>::_Single_object
+        make_unique(Args&&... args) {
+            return unique_ptr<T>(new T(std::forward<Args>(args)...));
+        }
+
+    template<class T>
+        typename _Unique_if<T>::_Unknown_bound
+        make_unique(size_t n) {
+            typedef typename remove_extent<T>::type U;
+            return unique_ptr<T>(new U[n]());
+        }
+
+    template<class T, class... Args>
+        typename _Unique_if<T>::_Known_bound
+        make_unique(Args&&...) = delete;
+  }
+#endif
+
 bool DEBUG = false;
 
 // Model Specification for Kaldi ASR
