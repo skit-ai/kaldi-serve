ea6d9e009a57ec0dbfcddc030659b7b70c516d85
Prabhsimran Singh <pskrunner14@gmail.com>
2020-06-13 05:56:45 +0530

    added decoder queue ctx manager and gil release calls for long running cpp

--- python/kaldiserve/__init__.py
+++ python/kaldiserve/__init__.py
@@ -3,3 +3,14 @@ from kaldiserve.kaldiserve_pybind import _ModelSpecList, _WordList, _Alternative
 from kaldiserve.kaldiserve_pybind import ChainModel                                         # models
 from kaldiserve.kaldiserve_pybind import Decoder, DecoderQueue, DecoderFactory              # decoders
 from kaldiserve.kaldiserve_pybind import parse_model_specs                                  # utils
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def acquire_decoder(dq: DecoderQueue):
+    decoder = dq.acquire()
+    try:
+        yield decoder
+    finally:
+        dq.release(decoder)
--- python/kaldiserve_pybind/decoder/decoder.cpp
+++ python/kaldiserve_pybind/decoder/decoder.cpp
@@ -28,35 +28,51 @@ void pybind_decoder(py::module &m) {
         // wav stream chunk
         .def("decode_stream_wav_chunk", [](Decoder &self, py::bytes &wav_bytes) {
             std::string wav_bytes_str(wav_bytes);
-            std::istringstream wav_stream(wav_bytes_str);
-            self.decode_stream_wav_chunk(wav_stream);
+            {
+                py::gil_scoped_release release;
+                std::istringstream wav_stream(wav_bytes_str);
+                self.decode_stream_wav_chunk(wav_stream);
+            }
+            
         })
         // raw wav stream chunk
         .def("decode_stream_raw_wav_chunk", [](Decoder &self, py::bytes &wav_bytes,
                                                const float &samp_freq, const int &data_bytes) {
             std::string wav_bytes_str(wav_bytes);
-            std::istringstream wav_stream(wav_bytes_str);
-            self.decode_stream_raw_wav_chunk(wav_stream, samp_freq, data_bytes);
+            {
+                py::gil_scoped_release release;
+                std::istringstream wav_stream(wav_bytes_str);
+                self.decode_stream_raw_wav_chunk(wav_stream, samp_freq, data_bytes);
+            }
         })
         // wav audio
         .def("decode_wav_audio", [](Decoder &self, py::bytes &wav_bytes, const float &chunk_size) {
             std::string wav_bytes_str(wav_bytes);
-            std::istringstream wav_stream(wav_bytes_str);
-            self.decode_wav_audio(wav_stream, chunk_size);
+            {
+                py::gil_scoped_release release;
+                std::istringstream wav_stream(wav_bytes_str);
+                self.decode_wav_audio(wav_stream, chunk_size);
+            }
         }, py::arg("wav_bytes"), py::arg("chunk_size") = 1.0)
         // raw wav audio
         .def("decode_raw_wav_audio", [](Decoder &self, py::bytes &wav_bytes, const float &samp_freq,
                                         const int &data_bytes, const float &chunk_size) {
             std::string wav_bytes_str(wav_bytes);
-            std::istringstream wav_stream(wav_bytes_str);
-            self.decode_raw_wav_audio(wav_stream, samp_freq, data_bytes, chunk_size);
+            {
+                py::gil_scoped_release release;
+                std::istringstream wav_stream(wav_bytes_str);
+                self.decode_raw_wav_audio(wav_stream, samp_freq, data_bytes, chunk_size);
+            }
         }, py::arg("wav_bytes"), py::arg("samp_freq"),
            py::arg("data_bytes"), py::arg("chunk_size") = 1.0)
         // get decoding results -> list[Alternative]
         .def("get_decoded_results", [](Decoder &self, const int &n_best,
                                        const bool &word_level, const bool &bidi_streaming) {
             std::vector<Alternative> alts;
-            self.get_decoded_results(n_best, alts, word_level, bidi_streaming);
+            {
+                py::gil_scoped_release release;
+                self.get_decoded_results(n_best, alts, word_level, bidi_streaming);
+            }
             py::list py_alts = py::cast(alts);
             return py_alts;
         }, py::arg("n_best"),
